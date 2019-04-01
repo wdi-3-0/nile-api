d3f6623b8d92fb2d112a27b41f408271d707e7c1
TaraYoo <yootar@gmail.com>
2019-04-01 13:16:55 -0400

    Move Purchase.create to .catch
    Move cart creation to .catch so that when there's an error, get /cart
    will try creating a cart before passing along an error

--- app/routes/cart_routes.js
+++ app/routes/cart_routes.js
@@ -30,33 +30,30 @@ router.get('/cart', requireToken, (req, res, next) => {
       if (err) throw err
       return product
     })
-    .then(cart => {
-      if (cart === null) {
-        req.body = {purchase: {}}
-        req.body.purchase.owner = req.user.id
-        Purchase.create(req.body.purchase, function (err, cart) {
-          if (err) console.log(err)
-          return cart
-        })
-          .then(cart => {
-            return cart
-          })
-        // respond to succesful `create` with status 201 and JSON of new "cart"
-          // but logs type of this item as undefined
-          .then(cart => {
-            res.status(201).json({ cart })
-          })
-          // if an error occurs, pass it off to our error handler
-          // the error handler needs the error message and the `res` object so that it
-          // can send an error message back to the client
-          .catch(next)
-      }
-      return cart
-    })
-    .then(cart => res.status(200).json({ cart }))
+    .then(cart => res.status(200).json({ cart: cart.toObject() }))
 
     // if an error occurs, pass it to the handler
-    .catch(next)
+    .catch(() => {
+      req.body.purchase = {}
+      req.body.purchase.owner = req.user.id
+      Purchase.create(req.body.purchase, function (err, cart) {
+        if (err) console.log(err)
+        return cart
+      })
+
+        .then(cart => {
+          return cart
+        })
+      // respond to succesful `create` with status 201 and JSON of new "cart"
+        // but logs type of this item as undefined
+        .then(cart => {
+          res.status(201).json({ cart: cart.toObject() })
+        })
+        // if an error occurs, pass it off to our error handler
+        // the error handler needs the error message and the `res` object so that it
+        // can send an error message back to the client
+        .catch(next)
+    })
 })
 
 // CREATE
